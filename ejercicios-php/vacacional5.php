3781f8fb162cb98b63b78362487c5b24f726c6a7
AlGonzalezGuardiola <algonzalezguardiola@gmail.com>
2024-01-19 20:28:57 +0100

    Guardando cambios en vacacional5.php antes de fusionar

--- ejercicios-php/vacacional5.php
+++ ejercicios-php/vacacional5.php
@@ -8,4 +8,28 @@ if (!$xml = file_get_contents($url)) {
 }
 
 echo $xml;
-?>
+
+
+$municipis = array();
+
+$dades = $xml->rows;
+$i = 0;
+
+foreach ($xml->rows->row as $dades) {
+	if (isset($dades->municipi)) {
+        $municipis[$i] = $dades->municipi;
+	$i++;
+    }
+  }
+  ?>
+
+
+<form method="GET" action="">
+
+	<label for="municipi">Municipio:</label>
+	<select name="municipi">
+		<option value="" <?php echo $municipis === "" ? "selected" : ""; ?>>Todos los municipios</option>
+			 <option value= <?php $municipis[$i];
+				for ($i = 0; $i < count($municipis); $i++){
+}
+?>>
